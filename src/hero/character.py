e098443d6b8f8e450472494ab6344a6701f775a9
ocorvu <professorjoaodamata@gmail.com>
2022-10-07 19:15:36 -0300

    arquivo character.py criado com as classes Character e Hero

--- src/hero/character.py
+++ src/hero/character.py
@@ -1,13 +1,18 @@
 from random import randint
-class Hero:
-    def __init__(self, name: str, race: str, classe: str, hp: int, armour: int, lvl: int = 1, power: int = 1):#alguns atributos como hp e power serão predefinidos de acordo com a classe, raça e lvl.
+class Character:
+    """
+    Um Personagem com: Nome, HP, Armour, Level e Power.
+    """
+    def __init__(self, name: str, hp: int, armour: int, lvl: int = 1, power: int = 1):#alguns atributos como hp e power serão predefinidos de acordo com a classe, raça e lvl.
         self.name = name
-        self.classe = classe
-        self.raca = race
         self.level = lvl
         self.power = power
         self.hp = hp
         self.armour = armour
+
+    def __str__(self) -> str:
+        return f'{self.name} - LVL {self.level} -  {self.hp} HP - {self.power} Power - {self.armour} Armor'
+
     def atack(self, critChance = 0, critMult = 1):#crit chance e mult serao buildados junto a classe, sendo dispensado a parametrização.
         power = self.power
         isCrit = critChance - randint(0,100)
@@ -18,16 +23,18 @@ class Hero:
         damage = power + power*isCrit*critMult
         return damage
 
-class Enemy: # talvez seja interessante deixar o enemy na mesma classe do hero para terem os mesmos atributos
-    def __init__(self, name, hp, armour, power):
-        self.name = name
-        self.hp = hp
-        self.armour = armour
-        self.power = power
-    def atack(self):
-        return self.power
 
-def fight(hero: Hero, target: Enemy):
+class Hero(Character):
+    """Um Personagem com Classe e Raça"""
+    def __init__(self, name: str, race: str, classe: str, hp: int, armour: int, lvl: int = 1, power: int = 1):
+        super().__init__(name, hp, armour, lvl, power)
+        self.race = race
+        self.classe = classe
+    
+    def __str__(self) -> str:
+        return f'{self.name} - LVL {self.level} - {self.race} - {self.classe} -  {self.hp} HP - {self.power} Power - {self.armour} Armor'
+
+def fight(hero: Character, target: Character):
     """
     O combate ocorre sempre entre dois personagens.
     Será encerrado quando o número máximo de rodadas
@@ -52,13 +59,6 @@ def fight(hero: Hero, target: Enemy):
     if target.hp <= 0:
         print(target.name,'died')
 
+vayne = Character('Shauna', 100, 2)
+shauna = Hero('Vayne', 'Humana', 'Atirador', 100, 2)
 
-
-
-#exemplo com diferentes crit chances
-vayne = Hero('vayne','human','archer',100,5,'a',5)
-print(vayne.level)
-
-#lutando contra monstro
-slime = Enemy('slime',100,1,5)
-fight(vayne,slime)
