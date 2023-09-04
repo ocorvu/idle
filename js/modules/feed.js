22391b825fae0d674f2e0cda364673b71d9ddefb
ocorvu <professorjoaodamata@gmail.com>
2023-09-04 13:50:12 -0300

    Data da 'atividade' agora é responsabilidade do feed

--- js/modules/feed.js
+++ js/modules/feed.js
@@ -1,7 +1,15 @@
 function newActivity(feed, activity) {
     const paragraph = document.createElement('p');
 
-    paragraph.innerText = activity
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+
+    paragraph.innerText = `[${day}/${month}/${year} ${hours}:${minutes}:${seconds}] ${activity}`
     paragraph.classList.add('activity')
 
     feed.prepend(paragraph)
