7ec395cad4ceff88709509674bb50e510c14e76c
ocorvu <professorjoaodamata@gmail.com>
2023-10-01 15:25:50 -0300

    adicionada propridade totalHp e formatação de documento

--- js/modules/character.js
+++ js/modules/character.js
@@ -1,6 +1,7 @@
 class Character {
     constructor(name, hp = 100, atk, def, thumbnail = '') {
         this.name = name;
+        this.totalHp = hp;
         this.hp = hp;
         this.atk = atk;
         this.def = def;
@@ -9,13 +10,14 @@ class Character {
     }
     self() {
         return {
-         "name": this.name,
-         "hp": this.hp,
-         "atk": this.atk,
-         "def": this.def,
-         "thumbnail": this.thumbnail,
+            "name": this.name,
+            "totalHp": this.hp,
+            "hp": this.hp,
+            "atk": this.atk,
+            "def": this.def,
+            "thumbnail": this.thumbnail,
         }
     }
 }
 
-export {Character}
+export { Character }
