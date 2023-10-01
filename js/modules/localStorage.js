b7228b949b245aa49bf5eec635cafcd6081bfa77
ocorvu <professorjoaodamata@gmail.com>
2023-10-01 15:39:42 -0300

    formatação de documento

--- js/modules/localStorage.js
+++ js/modules/localStorage.js
@@ -3,24 +3,23 @@ import { Hero } from './hero.js';
 
 import data from '../../data/characters.json' assert { type: 'json' };
 
-const heroesLocal = []
-const enemiesLocal = []
+const heroesLocal = [];
+const enemiesLocal = [];
 
 data.heroes.forEach(hero => {
     heroesLocal[hero.name.toLocaleLowerCase()] = new Hero(hero.name, hero.hp, hero.atk, hero.def, hero.thumbnail, hero.level, hero.power, hero.given_power, hero.base_cost, hero.cost_increase, hero.require);
 });
 
 data.enemies.forEach(enemie => {
-    enemiesLocal[enemie.name.toLocaleLowerCase()] = new Character(enemie.name, enemie.hp, enemie.atk, enemie.def, enemie.thumbnail)
+    enemiesLocal[enemie.name.toLocaleLowerCase()] = new Character(enemie.name, enemie.hp, enemie.atk, enemie.def, enemie.thumbnail);
 });
 
-let characters =
-{
+let characters = {
     heroes: {},
     enemies: {}
 }
 
-let obj = ''
+let obj = '';
 
 for (let hero in heroesLocal) {
     obj = Object.assign(characters.heroes, {
@@ -34,8 +33,6 @@ for (let enemie in enemiesLocal) {
     });
 }
 
-let items = localStorage.setItem('characters', JSON.stringify(characters)) 
+let items = localStorage.setItem('characters', JSON.stringify(characters));
 
-// localStorage.clear()
-
-export {heroesLocal, enemiesLocal}
+export { heroesLocal, enemiesLocal }
