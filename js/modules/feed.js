eed800fe6137cac6aca56591cd42611c05379ee4
ocorvu <professorjoaodamata@gmail.com>
2023-11-15 20:00:57 -0300

    separação da data em uma nova função

--- js/modules/feed.js
+++ js/modules/feed.js
@@ -1,18 +1,22 @@
+function dateMinutesSeconds() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `[${day}/${month}/${year} ${hours}:${minutes}:${seconds}] `;
+}
+
 function newActivity(feed, activity) {
     const paragraph = document.createElement('p');
 
-    const date = new Date()
-    const day = date.getDate()
-    const month = date.getMonth() + 1
-    const year = date.getFullYear()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
+    paragraph.innerText = `${dateMinutesSeconds()} ${activity}`;
+    paragraph.classList.add('activity');
 
-    paragraph.innerText = `[${day}/${month}/${year} ${hours}:${minutes}:${seconds}] ${activity}`
-    paragraph.classList.add('activity')
-
-    feed.prepend(paragraph)
+    feed.prepend(paragraph);
 
 }
 
