3dbea223e6747f6338e2a195e765495257826325
ocorvu <professorjoaodamata@gmail.com>
2023-11-25 22:40:18 -0300

    refatoração para que seja funcional e independente de variáveis locais

--- js/modules/localStorage.js
+++ js/modules/localStorage.js
@@ -1,46 +1,34 @@
+import { requestData } from './functions.js';
 import { Character } from './character.js';
 import { Hero } from './hero.js';
 
-const heroesLocal = [];
-const enemiesLocal = [];
+async function createCharacters(path) {
+    const data = await requestData(path);
 
-async function requestData(path) {
-    let obj;
-  
-    const res = await fetch(path);
-  
-    obj = await res.json();
-  
-    return obj
-  }
-  
-const data = await requestData("data/characters.json");
+    const characters = {
+        heroes: {},
+        enemies: {},
+    };
 
-data.heroes.forEach(hero => {
-    heroesLocal[hero.name.toLocaleLowerCase()] = new Hero(hero.name, hero.totalHp, hero.hp, hero.atk, hero.def, hero.thumbnail, hero.level, hero.power, hero.given_power, hero.base_cost, hero.cost_increase, hero.require);
-});
-
-data.enemies.forEach(enemie => {
-    enemiesLocal[enemie.name.toLocaleLowerCase()] = new Character(enemie.name, enemie.totalHp, enemie.hp, enemie.atk, enemie.def, enemie.thumbnail);
-});
-
-let characters = {
-    heroes: {},
-    enemies: {}
-}
-
-let obj = '';
-
-for (let hero in heroesLocal) {
-    obj = Object.assign(characters.heroes, {
-        [hero]: heroesLocal[hero]
+    data.heroes.forEach(hero => {
+        Object.assign(characters.heroes, {
+            [hero.name.toLowerCase()]: new Hero(hero.name, hero.totalHp, hero.hp, hero.atk, hero.def, hero.thumbnail, hero.level, hero.power, hero.given_power, hero.base_cost, hero.cost_increase, hero.require)
+        })
     });
-}
 
-for (let enemie in enemiesLocal) {
-    obj = Object.assign(characters.enemies, {
-        [enemie]: enemiesLocal[enemie]
+    data.enemies.forEach(enemie => {
+        Object.assign(characters.enemies, {
+            [enemie.name.toLowerCase()]: new Character(enemie.name, enemie.totalHp, enemie.hp, enemie.atk, enemie.def, enemie.thumbnail)
+        })
     });
+
+    return characters;
 }
 
-let items = localStorage.setItem('characters', JSON.stringify(characters));
+async function load(file) {
+    const characters = await createCharacters(file);
+
+    const save = localStorage.setItem('characters', JSON.stringify(characters));
+}
+
+export {load}
