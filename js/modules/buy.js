49a847419cdf3696b02986d286fd62eac7b27d65
ocorvu <professorjoaodamata@gmail.com>
2024-07-04 09:24:28 -0300

    Incluído lógica para opção de upgrade 'max' e generalização de opções númericas

--- js/modules/buy.js
+++ js/modules/buy.js
@@ -1,5 +1,26 @@
-function ten(hero, option) {
+let maxCost = 0;
+
+function numberOption(hero, option) {
     return (hero.base_cost * ((hero.cost_increase ** option) - 1)) / (hero.cost_increase - 1);
 }
+function max(hero, points, level) {
+    if (numberOption(hero, level) >= points) {
+        maxCost = 0;
+        return level == 1 ? level : level - 1;
+    }
+    maxCost += numberOption(hero, level);
+    level += 1;
+    return max(hero, points, level);
+}
 
-export { ten }
+function buy(hero, option, points) {
+    switch (option) {
+        case 'max':
+            let newOption = max(hero, points, 1);
+            return [numberOption(hero, newOption), newOption]
+        default:
+            return [numberOption(hero, option), option];
+    }
+}
+
+export { buy }
